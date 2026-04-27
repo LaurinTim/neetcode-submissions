41c8292ca5c6c5d4e13391065cef9d0d13e607e5
Laurin Koller <laurin.koller@gmail.com>
2026-04-27 16:43:30 +0200

    Add: hashTable - submission-0

---
+++ Data Structures & Algorithms/hashTable/submission-0.py
@@ -0,0 +1,36 @@
+class HashTable:
+    
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hash_map = {}
+
+
+    def insert(self, key: int, value: int) -> None:
+        if key in self.hash_map.keys():
+            self.hash_map[key] = value
+        else:
+            if self.getSize() + 1 >= self.capacity/2:
+                self.resize()
+            self.hash_map[key] = value
+
+
+    def get(self, key: int) -> int:
+        return self.hash_map.get(key, -1)
+
+
+    def remove(self, key: int) -> bool:
+        removed_key = self.hash_map.pop(key, None)
+        return removed_key is not None
+
+
+    def getSize(self) -> int:
+        return len(self.hash_map)
+
+
+    def getCapacity(self) -> int:
+        return self.capacity
+
+
+    def resize(self) -> None:
+        self.capacity *= 2
+
