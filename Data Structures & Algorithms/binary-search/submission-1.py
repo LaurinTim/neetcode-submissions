bbd9b244f77b457000eb514ab83ceb008a7d3956
Laurin Koller <laurin.koller@gmail.com>
2026-05-31 08:08:15 +0200

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+        m = r // 2
+
+        while l <= r:
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                return m
+            m = l + (r - l) // 2
+
+        return -1
+        
