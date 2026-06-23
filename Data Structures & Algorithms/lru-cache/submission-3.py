4b8625dd717ca5a21325cc674d9ef464596d2ac0
Laurin Koller <laurin.koller@gmail.com>
2026-06-23 20:59:58 +0200

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+        self.head = Node(None, None)
+        self.tail = Node(None, None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            curr_node = self.cache[key]
+            self.move_used_node(curr_node)
+            return curr_node.val
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            curr_node = self.cache[key]
+            self.cache[key].val = value
+            self.move_used_node(curr_node)
+            return
+        elif len(self.cache) == self.capacity:
+            self.cache.pop(self.head.next.key, None)
+            self.head.next.next.prev = self.head
+            self.head.next = self.head.next.next
+
+        new_node = Node(key, value)
+        self.cache[key] = new_node
+        new_node.prev = self.tail.prev
+        new_node.next = self.tail
+        self.tail.prev.next = new_node
+        self.tail.prev = new_node
+        if key == 20:
+            print(self.cache[2].next.key)
+            print(self.head.next.next.prev)
+        
+    def move_used_node(self, curr_node):
+        curr_node.prev.next = curr_node.next
+        curr_node.next.prev = curr_node.prev
+        curr_node.prev = self.tail.prev
+        curr_node.next = self.tail
+        self.tail.prev.next = curr_node
+        self.tail.prev = curr_node
+        
+        
