054174b79e0ea290b466347de44540fa4493b5e0
Laurin Koller <laurin.koller@gmail.com>
2026-08-09 16:17:54 +0200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_hash = dict()
+        for letter in s:
+            if letter in s_hash:
+                s_hash[letter] += 1
+            else:
+                s_hash[letter] = 1
+        
+        for letter in t:
+            if letter in s_hash:
+                if s_hash[letter] == 0:
+                    return False
+                else:
+                    s_hash[letter] -= 1
+            else:
+                return False
+        return True
